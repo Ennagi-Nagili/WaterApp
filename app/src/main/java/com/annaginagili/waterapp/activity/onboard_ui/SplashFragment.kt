ae8b25376067022738670ce91a665a75f16b8ef5
ravankhidirov <ravankhidirov@gmail.com>
2023-10-08 12:00:39 +0400

    My commit 12:00

--- app/src/main/java/com/annaginagili/waterapp/activity/onboard_ui/SplashFragment.kt
+++ app/src/main/java/com/annaginagili/waterapp/activity/onboard_ui/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.annaginagili.waterapp.activity.onboard_ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.annaginagili.waterapp.databinding.FragmentSplashBinding
 
 
@@ -25,6 +28,12 @@ class SplashFragment : Fragment() {
     ): View? {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+        }, 2000)
 
 
         return root
